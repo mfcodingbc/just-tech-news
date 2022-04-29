4c6746eeff9c9ee9f11a522696e09118dcc8c9fb
Mitchell Frey <mfcodingbc@gmail.com>
2022-04-28 23:58:17 -0400

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -4,11 +4,17 @@ const Sequelize = require('sequelize');
 // import the environment variables (to prevent sensitive info from being uploaded)
 require('dotenv').config();
 
-// create connection to the      database, using your username, password
-const sequelize = new Sequelize('just_tech_news_db', 'root', 'My$QLP@55w0rd', {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
-});
+// create connection to the database for use with JawsDB on Heroku
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
 
 module.exports = sequelize;
