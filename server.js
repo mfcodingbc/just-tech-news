4b7ccfa2b7d4549cd513e1d3e154dfcc9315db22
Mitchell Frey <mfcodingbc@gmail.com>
2022-05-05 18:35:54 -0400

    Code refactor, partials created, tests created

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const path = require('path');
 const session = require('express-session');
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
 
 // creating sessions for cookies
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -19,7 +21,7 @@ const sess = {
 
 // handlebars.js setup
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
